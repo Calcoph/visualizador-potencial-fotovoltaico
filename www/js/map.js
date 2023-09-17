83237a1dca882ed2600323e374cc956232763b65
Calcoph <34724728+Calcoph@users.noreply.github.com>
2023-09-17 21:08:11 +0200

    servidor http

--- www/js/map.js
+++ www/js/map.js
@@ -1,6 +1,6 @@
 /// <reference path="../../typings/index.d.ts" />
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([43.2629, -2.95], 18);
 
 /* TODO: Read https://operations.osmfoundation.org/policies/tiles/ */
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -8,4 +8,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+//var marker = L.marker([43.2629, -2.95]).addTo(map);
+
+fetch("./geojson/TODO")
+    .then(response => response.json())
+    .then((json) => {
+        console.log(typeof(json))
+        L.geoJSON(json).addTo(map)
+    })
