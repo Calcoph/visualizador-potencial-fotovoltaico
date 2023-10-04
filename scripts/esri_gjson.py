c7a1b96ea6431fc9af1d7d17c2117e2489f6202f
Diego <34724728+Calcoph@users.noreply.github.com>
2023-10-04 11:26:31 +0200

    script transformador de esri a geojson

---
+++ scripts/esri_gjson.py
@@ -0,0 +1,23 @@
+# (en bash) export PYTHONPATH=/usr/lib/python3/dist-packages
+# (en powershell) $Env:PYTHONPATH="/usr/lib/python3/dist-packages"
+
+from qgis.core import QgsApplication, QgsProject, QgsVectorLayer, QgsMapLayer, QgsVectorFileWriter, QgsCoordinateReferenceSystem
+
+
+def main(qgs: QgsApplication, layer_path: str):
+    layer: QgsVectorLayer = QgsVectorLayer(layer_path, "name", "ogr")
+    crs = QgsCoordinateReferenceSystem("EPSG:4326")
+    QgsVectorFileWriter.writeAsVectorFormat(layer, "a.geojson", "utf-8", crs, "GeoJSON")
+
+if __name__ == "__main__":
+
+    QgsApplication.setPrefixPath("/usr", True)
+
+    qgs = QgsApplication([], False)
+
+    qgs.initQgis()
+
+    main(qgs, "ignore/output_20220503_eraikin bi_1/EIB_epv_year_filt.shp")
+
+    qgs.exitQgis()
+
