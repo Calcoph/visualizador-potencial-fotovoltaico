fdb7657037c685ab979f7aa131ef3497a484f756
Diego <34724728+Calcoph@users.noreply.github.com>
2023-10-18 11:33:17 +0200

    Añadir interfaz prototipo

--- www/js/map.js
+++ www/js/map.js
@@ -1,18 +1,42 @@
 /// <reference path="../../typings/index.d.ts" />
 
-var map = L.map('map').setView([43.2629, -2.95], 18);
+function init() {
+    init_map()
+}
 
-/* TODO: Read https://operations.osmfoundation.org/policies/tiles/ */
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+function init_map() {
+    var map = L.map('map').setView([43.2629, -2.95], 18);
+    
+    /* TODO: Read https://operations.osmfoundation.org/policies/tiles/ */
+    /* L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map); */
+    
+    //var marker = L.marker([43.2629, -2.95]).addTo(map);
+    
+    fetch("./geojson/a.geojson")
+        .then(response => response.json())
+        .then((json) => {
+            L.geoJSON(json).addTo(map)
+        })
+}
 
-//var marker = L.marker([43.2629, -2.95]).addTo(map);
-
-fetch("./geojson/TODO")
-    .then(response => response.json())
-    .then((json) => {
-        console.log(typeof(json))
-        L.geoJSON(json).addTo(map)
-    })
+/**
+ * @param {String} tab
+ */
+function change_tab(tab) {
+    switch (tab) {
+        case "atributos":
+            console.log(1)
+            break
+        case "marcadores":
+            console.log(2)
+            break
+        case "mis edificios":
+            console.log(3)
+            break
+        default:
+            break
+    }
+}
