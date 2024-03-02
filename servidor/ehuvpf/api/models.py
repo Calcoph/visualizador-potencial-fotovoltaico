38a3f0655aeb55b0624aca1283abb3a26c864ece
Calcoph <34724728+Calcoph@users.noreply.github.com>
2024-03-02 01:31:28 +0100

    api PoC

--- servidor/ehuvpf/api/models.py
+++ servidor/ehuvpf/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.TextField()
+
+class Building(models.Model):
+    project = models.OneToOneField(
+        Project,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+    path = models.TextField()
+    lat = models.IntegerField()
+    lon = models.IntegerField()
--- servidor/ehuvpf/api/views.py
+++ servidor/ehuvpf/api/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def get_buildings(request: HttpRequest):
+    params = request.GET
+    lat = params.get("lat")
+    lon = params.get("lon")
+    return HttpResponse(f"Has pedido los edificios de lat:{lat} lon:{lon}")
